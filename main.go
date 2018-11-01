b11b0d55afc56fed8d9b8b889c9c1db2144b551e
Chris Nyaga <nyagachris75@gmail.com>
2018-11-01 23:45:52 +0300

    learnt how to create custom types

--- main.go
+++ main.go
@@ -1,15 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Spades", newCard()}
-	cards = append(cards, "The Joker")
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	deck := newDeck()
+	deck.print()
 }
